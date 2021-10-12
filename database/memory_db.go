3c3c9072a277246c66f5e177d97c2632ca552739
pieter <tpho.skp@edu.mercantec.dk>
2021-10-12 16:44:06 +0200

    fix issues and renaming

--- database/memory_db.go
+++ database/memory_db.go
@@ -2,26 +2,32 @@ package database
 
 import (
 	"fmt"
-	"neocheckin_cache/database/database_models"
+	"neocheckin_cache/database/models"
 	"neocheckin_cache/shared"
 )
 
 type MemoryDatabase struct {
 	AbstractDatabase
-	employees []database_models.Employee
-	options   []database_models.Option
+	employees []models.Employee
+	options   []models.Option
 }
 
-func find[T any](a []T, f func(T) bool) T, error {
+func findEmployee(a []models.Employee, f func(models.Employee) bool) (models.Employee, error) {
 	for _, v := range a {
 		if f(v) {
-			return v
+			return v, nil
 		}
 	}
 	panic("Not found")
 }
 
-func (db *MemoryDatabase) GetEmployeeWithRfid(rfid string) (database_models.Employee, error) {
+func (db *MemoryDatabase) GetEmployeeWithRfid(rfid string) (models.Employee, error) {
+	employees, err := findEmployee(db.employees, func(model models.Employee) bool {
+		return true
+	})
+
+	print(employees, err)
+
 	for i := range db.employees {
 		if db.employees[i].Rfid == rfid {
 			return db.employees[i], nil
@@ -30,42 +36,42 @@ func (db *MemoryDatabase) GetEmployeeWithRfid(rfid string) (database_models.Empl
 	panic(fmt.Sprintf("Could not find Employee with rfid '%s'", rfid))
 }
 
-func (db *MemoryDatabase) GetEmployeeWithDatabaseId(string) database_models.Employee {
+func (db *MemoryDatabase) GetEmployeeWithDatabaseId(string) models.Employee {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) InsertEmployee(database_models.Employee) error {
+func (db *MemoryDatabase) InsertEmployee(models.Employee) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) UpdateEmployeeWithDatabaseId(string, database_models.Employee) error {
+func (db *MemoryDatabase) UpdateEmployeeWithDatabaseId(string, models.Employee) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) DeleteEmployeeWithDatabaseId(string, database_models.Employee) error {
+func (db *MemoryDatabase) DeleteEmployeeWithDatabaseId(string, models.Employee) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) GetOptionWithWrapperId(shared.WrapperEnum) (database_models.Option, error) {
+func (db *MemoryDatabase) GetOptionWithWrapperId(shared.WrapperEnum) (models.Option, error) {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) GetOptionWithDatabaseId(string) (database_models.Option, error) {
+func (db *MemoryDatabase) GetOptionWithDatabaseId(string) (models.Option, error) {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) InsertOption(database_models.Option) error {
+func (db *MemoryDatabase) InsertOption(models.Option) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) UpdateOptionWithDatabaseId(string, database_models.Option) error {
+func (db *MemoryDatabase) UpdateOptionWithDatabaseId(string, models.Option) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) DeleteOptionWithDatabaseId(string, database_models.Option) error {
+func (db *MemoryDatabase) DeleteOptionWithDatabaseId(string, models.Option) error {
 	panic("Not implemented")
 }
 
-func (db *MemoryDatabase) AddAction(database_models.Action) {
+func (db *MemoryDatabase) AddAction(models.Action) {
 	panic("Not implemented")
 }
