18cf170ea3d8f1fe149e4ad15da136cd9f736818
pieter <tpho.skp@edu.mercantec.dk>
2021-10-12 16:43:48 +0200

    rename /database_models => /models

--- database/abstract_db.go
+++ database/abstract_db.go
@@ -1,22 +1,22 @@
 package database
 
 import (
-	"neocheckin_cache/database/database_models"
+	"neocheckin_cache/database/models"
 	"neocheckin_cache/shared"
 )
 
 type AbstractDatabase interface {
-	GetEmployeeWithRfid(string) (database_models.Employee, error)
-	GetEmployeeWithDatabaseId(string) database_models.Employee
-	InsertEmployee(database_models.Employee) error
-	UpdateEmployeeWithDatabaseId(string, database_models.Employee) error
-	DeleteEmployeeWithDatabaseId(string, database_models.Employee) error
+	GetEmployeeWithRfid(string) (models.Employee, error)
+	GetEmployeeWithDatabaseId(string) models.Employee
+	InsertEmployee(models.Employee) error
+	UpdateEmployeeWithDatabaseId(string, models.Employee) error
+	DeleteEmployeeWithDatabaseId(string, models.Employee) error
 
-	GetOptionWithWrapperId(shared.WrapperEnum) (database_models.Option, error)
-	GetOptionWithDatabaseId(string) (database_models.Option, error)
-	InsertOption(database_models.Option) error
-	UpdateOptionWithDatabaseId(string, database_models.Option) error
-	DeleteOptionWithDatabaseId(string, database_models.Option) error
+	GetOptionWithWrapperId(shared.WrapperEnum) (models.Option, error)
+	GetOptionWithDatabaseId(string) (models.Option, error)
+	InsertOption(models.Option) error
+	UpdateOptionWithDatabaseId(string, models.Option) error
+	DeleteOptionWithDatabaseId(string, models.Option) error
 
-	AddAction(database_models.Action) error
+	AddAction(models.Action) error
 }
