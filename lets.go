2899b09c130333f5e351526df41a5e88e4b8542a
pieter <tpho.skp@edu.mercantec.dk>
2021-10-13 10:41:45 +0200

    connect routes

--- lets.go
+++ lets.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"neocheckin_cache/database"
+	"neocheckin_cache/database/models"
+	"neocheckin_cache/router"
 	"net/http"
 )
 
 func main() {
+	db := database.MemoryDatabase{}
+	db.InsertEmployee(models.Employee{
+		Rfid:       "rfid",
+		Name:       "rfid",
+		Flex:       50,
+		Working:    true,
+		Department: "rfid",
+		Photo:      "rfid",
+	})
+	router := router.ConnectAPI(&db)
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		router.Handle(rw, *r, &db)
+	})
 	http.ListenAndServe(":8079", nil)
 }
