a63c985f57fde7f5e4209c34f496bebd291de369
pieter <tpho.skp@edu.mercantec.dk>
2021-10-13 08:17:31 +0200

    only use id to delete

--- database/abstract_db.go
+++ database/abstract_db.go
@@ -10,13 +10,13 @@ type AbstractDatabase interface {
 	GetEmployeeWithDatabaseId(string) models.Employee
 	InsertEmployee(models.Employee) error
 	UpdateEmployeeWithDatabaseId(string, models.Employee) error
-	DeleteEmployeeWithDatabaseId(string, models.Employee) error
+	DeleteEmployeeWithDatabaseId(string) error
 
 	GetOptionWithWrapperId(shared.WrapperEnum) (models.Option, error)
 	GetOptionWithDatabaseId(string) (models.Option, error)
 	InsertOption(models.Option) error
 	UpdateOptionWithDatabaseId(string, models.Option) error
-	DeleteOptionWithDatabaseId(string, models.Option) error
+	DeleteOptionWithDatabaseId(string) error
 
 	AddAction(models.Action) error
 }
