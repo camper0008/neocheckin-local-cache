97e180d21b0b5cb3eba9a16ebb7da094910a3ee1
pieter <tpho.skp@edu.mercantec.dk>
2021-10-13 09:31:59 +0200

    models

--- router/api/models/response_models/get_employee.go
+++ router/api/models/response_models/get_employee.go
@@ -3,5 +3,5 @@ package response_models
 import "neocheckin_cache/router/api/models/exported_models"
 
 type GetEmployee struct {
-	Employee []exported_models.Employee
+	Employee exported_models.Employee
 }
