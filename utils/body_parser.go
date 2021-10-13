2ea4f849b581c4d3bf5748baad40e22b3be0ada3
pieter <tpho.skp@edu.mercantec.dk>
2021-10-13 12:33:24 +0200

    body parser

---
+++ utils/body_parser.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"neocheckin_cache/router/api/models/request_models"
+	"net/http"
+)
+
+func ParseBody(r http.Request, m *request_models.CardScanned) error {
+	headerContentType := r.Header.Get("Content-Type")
+	if headerContentType != "application/json" {
+		return fmt.Errorf("invalid content type, got '%s', expected 'application/json'", headerContentType)
+	}
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(m)
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			return fmt.Errorf("bad request. Wrong Type provided for field %s" + unmarshalErr.Field)
+		} else {
+			return fmt.Errorf("bad request %s", err.Error())
+		}
+	}
+	return nil
+}
