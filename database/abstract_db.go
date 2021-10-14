0aba49f0bcd7276c388d9f9c0b1bf5c4746b0cc9
pieter <tpho.skp@edu.mercantec.dk>
2021-10-14 08:25:27 +0200

    update abstract db requirements

--- database/abstract_db.go
+++ database/abstract_db.go
@@ -8,16 +8,19 @@ import (
 type AbstractDatabase interface {
 	GetEmployeeWithRfid(string) (models.Employee, error)
 	GetEmployeeWithDatabaseId(string) (models.Employee, error)
+	GetAllEmployees(string) (models.Employee, error)
 	InsertEmployee(models.Employee) error
 	UpdateEmployeeWithDatabaseId(string, models.Employee) error
 	DeleteEmployeeWithDatabaseId(string) error
 
 	GetOptionWithWrapperId(shared.WrapperEnum) (models.Option, error)
 	GetOptionWithDatabaseId(string) (models.Option, error)
+	GetAllOptions(string) (models.Option, error)
 	InsertOption(models.Option) error
 	UpdateOptionWithDatabaseId(string, models.Option) error
 	DeleteOptionWithDatabaseId(string) error
 
 	AddAction(models.Action) error
+	GetAllAction(models.Action) error
 	DeleteActionWithDatabaseId(string, models.Action) error
 }
