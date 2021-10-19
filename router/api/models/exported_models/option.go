b7b5499cb941723228387e9910a6a173db2a34f8
pieter <tpho.skp@edu.mercantec.dk>
2021-10-19 09:34:45 +0200

    updated options expmodel to fit api

--- router/api/models/exported_models/option.go
+++ router/api/models/exported_models/option.go
@@ -1,7 +1,15 @@
 package exported_models
 
+type OptionAvailable int
+
+const (
+	NOT_AVAILABLE = 0
+	AVAILABLE     = 1
+	PRIORITY      = 2
+)
+
 type Option struct {
-	Id        int    `json:"id"`
-	Name      string `json:"name"`
-	Available bool   `json:"available"`
+	Id        int             `json:"id"`
+	Name      string          `json:"name"`
+	Available OptionAvailable `json:"available"`
 }
