3cc37aafbdfd7745d9ef0c35d393a24af5b1d919
pieter <tpho.skp@edu.mercantec.dk>
2021-10-19 11:01:36 +0200

    pass tests

--- router/api/api_test.go
+++ router/api/api_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"neocheckin_cache/database"
 	m "neocheckin_cache/database/models"
+	em "neocheckin_cache/router/api/models/exported_models"
 	"testing"
 )
 
@@ -108,12 +109,82 @@ func TestGetEmployeeFromRfid(t *testing.T) {
 
 }
 
+func convertedOptionIsEqual(o1 []em.Option, o2 []em.Option) bool {
+	if len(o1) != len(o2) {
+		return false
+	}
+	for i := 0; i < len(o1); i++ {
+		if o1[i] != o2[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func TestConvertOptionsToExportedModels(t *testing.T) {
 	{ // arrays should be same length
 		o := make([]m.Option, 24)
 		cO := ConvertOptionsToExportedModels(o)
 		if len(o) != len(cO) {
 			t.Error("should be same length")
+		}
+	}
+	{ // should convert to exported_models.Option
+		o := []m.Option{
+			{
+				WrapperId: 1,
+				Name:      "Option 1",
+				Schedule: m.Schedule{
+					From: m.ScheduleTime{
+						Second: 0,
+						Minute: 0,
+						Hour:   0,
+					},
+					To: m.ScheduleTime{
+						Second: 0,
+						Minute: 0,
+						Hour:   24,
+					},
+					Days: m.ScheduleDays{
+						Monday:    true,
+						Tuesday:   true,
+						Wednesday: true,
+						Thursday:  true,
+						Friday:    true,
+						Saturday:  true,
+						Sunday:    true,
+					},
+				},
+			},
+			{
+				WrapperId: 2,
+				Name:      "Option 2",
+				Schedule: m.Schedule{
+					From: m.ScheduleTime{},
+					To:   m.ScheduleTime{},
+					Days: m.ScheduleDays{},
+				},
+			},
+		}
+		expected := []em.Option{
+			{
+				Id:        1,
+				Name:      "Option 1",
+				Available: em.OptionAvailable(em.AVAILABLE),
+			},
+			{
+				Id:        2,
+				Name:      "Option 2",
+				Available: em.OptionAvailable(em.NOT_AVAILABLE),
+			},
+		}
+		cO := ConvertOptionsToExportedModels(o)
+		if len(o) != len(cO) {
+			t.Error("should be same length")
+		}
+		if !convertedOptionIsEqual(cO, expected) {
+			t.Error("should equal expected")
 		}
 	}
 }
--- router/api/get_options.go
+++ router/api/get_options.go
@@ -5,6 +5,7 @@ import (
 	dbm "neocheckin_cache/database/models"
 	em "neocheckin_cache/router/api/models/exported_models"
 	"net/http"
+	"time"
 )
 
 func GetOptionsEndpoint(rw http.ResponseWriter, rq http.Request, db db.AbstractDatabase) {
@@ -29,6 +30,43 @@ func GetOptionsEndpoint(rw http.ResponseWriter, rq http.Request, db db.AbstractD
 	//}
 }
 
+func optionIsDuringWeekday(d dbm.ScheduleDays, w time.Weekday) bool {
+	// could not find an easier way to do this sadly.
+	if w == time.Monday && d.Monday {
+		return true
+	} else if w == time.Tuesday && d.Tuesday {
+		return true
+	} else if w == time.Wednesday && d.Wednesday {
+		return true
+	} else if w == time.Thursday && d.Thursday {
+		return true
+	} else if w == time.Friday && d.Friday {
+		return true
+	} else if w == time.Saturday && d.Saturday {
+		return true
+	} else if w == time.Sunday && d.Sunday {
+		return true
+	}
+	return false
+}
+
+func scheduleTimeToSeconds(s dbm.ScheduleTime) int {
+	return s.Second + s.Minute*60 + s.Hour*60*60
+}
+
+func OptionIsAvailable(o dbm.Option) bool {
+	t := time.Now()
+	w := t.Weekday()
+	if optionIsDuringWeekday(o.Schedule.Days, w) {
+		h, m, s := t.Clock()
+		tS := s + m*60 + h*60*60
+		frS := scheduleTimeToSeconds(o.Schedule.From)
+		toS := scheduleTimeToSeconds(o.Schedule.To)
+
+		return frS <= tS && tS <= toS
+	}
+	return false
+}
 func GetOptions(db db.AbstractDatabase) ([]dbm.Option, error) {
 	o, err := db.GetAllOptions()
 	if err != nil {
@@ -37,8 +75,23 @@ func GetOptions(db db.AbstractDatabase) ([]dbm.Option, error) {
 	return o, nil
 }
 
-func ConvertOptionsToExportedModels(o []dbm.Option) []em.Option {
-	result := make([]em.Option, len(o))
+func ConvertOptionsToExportedModels(d []dbm.Option) []em.Option {
+	r := make([]em.Option, len(d))
+	for i := 0; i < len(d); i++ {
+		a := OptionIsAvailable(d[i])
+		oa := em.OptionAvailable(em.INVALID)
+		if a {
+			oa = em.OptionAvailable(em.AVAILABLE)
+		} else {
+			oa = em.OptionAvailable(em.NOT_AVAILABLE)
+		}
 
-	return result
+		r[i] = em.Option{
+			Id:        d[i].WrapperId,
+			Name:      d[i].Name,
+			Available: oa,
+		}
+	}
+
+	return r
 }
