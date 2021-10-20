9bc051e5ab2d07b7a43baba41de7175199e10080
pieter <tpho.skp@edu.mercantec.dk>
2021-10-20 12:03:30 +0200

    reformat

--- wrapper/models/exported_models/task.go
+++ wrapper/models/exported_models/task.go
@@ -1,14 +1,10 @@
 package exported_models
 
-import (
-	"time"
-)
-
 type Task struct {
-	TaskId    int       `json:"taskId"`
-	Name      int       `json:"name"`
-	Rfid      string    `json:"employeeId"`
-	PostKey   string    `json:"highLevelApiKey"`
-	SystemId  string    `json:"systemIdentifier"`
-	Timestamp time.Time `json:"timestamp"`
+	TaskId    int    `json:"taskId"`
+	Name      string `json:"name"`
+	Rfid      string `json:"rfid"`
+	PostKey   string `json:"highLevelApiKey"`
+	SystemId  string `json:"systemIdentifier"`
+	Timestamp string `json:"timestamp"` // ISO
 }
