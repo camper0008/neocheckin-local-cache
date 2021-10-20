4141e171b602e89ef84d7b62c76624cf991a8c5f
pieter <tpho.skp@edu.mercantec.dk>
2021-10-20 14:05:02 +0200

    test option

--- lets.go
+++ lets.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"neocheckin_cache/database"
 	m "neocheckin_cache/database/models"
 	"neocheckin_cache/router"
@@ -17,9 +18,37 @@ func main() {
 		Department: "rfid",
 		Photo:      "rfid",
 	})
+	db.InsertOption(m.Option{
+		WrapperId: 0,
+		Name:      "default",
+		Schedule: m.Schedule{
+			From: m.ScheduleTime{
+				Second: 0,
+				Minute: 0,
+				Hour:   0,
+			},
+			To: m.ScheduleTime{
+				Second: 59,
+				Minute: 59,
+				Hour:   23,
+			},
+			Days: m.ScheduleDays{
+				Monday:    true,
+				Tuesday:   true,
+				Wednesday: true,
+				Thursday:  true,
+				Friday:    true,
+				Saturday:  true,
+				Sunday:    true,
+			},
+		},
+	})
 	router := router.ConnectAPI(&db)
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		router.Handle(rw, *r, &db)
 	})
-	http.ListenAndServe(":8079", nil)
+	err := http.ListenAndServe(":6000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
