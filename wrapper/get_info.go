d6989ef8d2758f8f02f1ca7e927afe92a695b6df
pieter <tpho.skp@edu.mercantec.dk>
2021-10-22 10:02:28 +0200

    get wrapper/employees

--- wrapper/get_info.go
+++ wrapper/get_info.go
@@ -1,1 +1,46 @@
+// TODO: log all errors
 package wrapper
+
+import (
+	"neocheckin_cache/utils"
+	rm "neocheckin_cache/wrapper/models/response_models"
+	"net/http"
+)
+
+func GetTaskTypes() (rm.GetTaskTypes, error) {
+	// TODO: use config url
+	resp, err := http.Get("http://localhost:7000/api/tasks/types")
+	if err != nil {
+		return rm.GetTaskTypes{}, err
+	}
+
+	parsed := rm.GetTaskTypes{}
+	err = utils.ParseBody(utils.ParseableBody{
+		Body:   resp.Body,
+		Header: resp.Header,
+	}, &parsed)
+	if err != nil {
+		return rm.GetTaskTypes{}, err
+	}
+
+	return parsed, nil
+}
+
+func GetEmployees() (rm.GetEmployees, error) {
+	// TODO: use config url
+	resp, err := http.Get("http://localhost:7000/api/employees/all")
+	if err != nil {
+		return rm.GetEmployees{}, err
+	}
+
+	parsed := rm.GetEmployees{}
+	err = utils.ParseBody(utils.ParseableBody{
+		Body:   resp.Body,
+		Header: resp.Header,
+	}, &parsed)
+	if err != nil {
+		return rm.GetEmployees{}, err
+	}
+
+	return parsed, nil
+}
