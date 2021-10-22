65cc5ad0a6205cd280e5e5e7e69aa9d8b50e78c5
pieter <tpho.skp@edu.mercantec.dk>
2021-10-22 11:21:02 +0200

    use wrapper

--- lets.go
+++ lets.go
@@ -2,81 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"neocheckin_cache/database"
-	m "neocheckin_cache/database/models"
 	"neocheckin_cache/router"
+	w "neocheckin_cache/wrapper"
 	"net/http"
 )
 
+func synchronizeWrapperAndCache(db database.AbstractDatabase) error {
+
+	t, err := w.GetTaskTypes()
+	if err != nil {
+		return err
+	} else {
+		w.UpdateDbFromTaskTypes(db, t)
+	}
+
+	e, err := w.GetEmployees()
+	if err != nil {
+		return err
+	} else {
+		w.UpdateDbFromEmployees(db, e)
+	}
+
+	return nil
+
+}
+
 func main() {
-	// TODO: get data from wrapper/wrapper mock
-	// TODO: add config file for wrapper URL
 	db := database.MemoryDatabase{}
-	db.InsertEmployee(m.Employee{
-		Rfid:       "0",
-		Name:       "employee man from employee land",
-		Flex:       5000,
-		Working:    true,
-		Department: "employee department",
-		Photo:      "",
-	})
-	db.InsertOption(m.Option{
-		WrapperId: 0,
-		Name:      "default",
-		Schedule: m.Schedule{
-			From: m.ScheduleTime{
-				Second: 0,
-				Minute: 0,
-				Hour:   0,
-			},
-			To: m.ScheduleTime{
-				Second: 59,
-				Minute: 59,
-				Hour:   23,
-			},
-			Days: m.ScheduleDays{
-				Monday:    true,
-				Tuesday:   true,
-				Wednesday: true,
-				Thursday:  true,
-				Friday:    true,
-				Saturday:  true,
-				Sunday:    true,
-			},
-		},
-	})
-	db.InsertOption(m.Option{
-		WrapperId: 1,
-		Name:      "priority",
-		Priority:  true,
-		Schedule: m.Schedule{
-			From: m.ScheduleTime{
-				Second: 0,
-				Minute: 0,
-				Hour:   0,
-			},
-			To: m.ScheduleTime{
-				Second: 59,
-				Minute: 59,
-				Hour:   23,
-			},
-			Days: m.ScheduleDays{
-				Monday:    true,
-				Tuesday:   true,
-				Wednesday: true,
-				Thursday:  true,
-				Friday:    true,
-				Saturday:  true,
-				Sunday:    true,
-			},
-		},
-	})
+
+	syncErr := synchronizeWrapperAndCache(&db)
+	if syncErr != nil {
+		fmt.Printf("error occured synchronizing: %q\n", syncErr.Error())
+	}
+
 	router := router.ConnectAPI(&db)
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		router.Handle(rw, *r, &db)
 	})
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
 }
