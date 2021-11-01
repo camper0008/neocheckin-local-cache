26c5d3fab3082e7276312c8b9d657c95d95c73c2
pieter <tpho.skp@edu.mercantec.dk>
2021-11-01 13:58:38 +0100

    print during startup

--- lets.go
+++ lets.go
@@ -9,33 +9,29 @@ import (
 	"net/http"
 )
 
-func synchronizeWrapperAndCache(db database.AbstractDatabase) error {
+func synchronizeWrapperAndCache(db database.AbstractDatabase) {
 
 	t, err := w.GetTaskTypes()
 	if err != nil {
-		return err
+		fmt.Printf("Error synchronizing task types: %v\n", err)
 	} else {
 		w.UpdateDbFromTaskTypes(db, t)
 	}
 
 	e, err := w.GetEmployees()
 	if err != nil {
-		return err
+		fmt.Printf("Error synchronizing employees: %v\n", err)
 	} else {
 		w.UpdateDbFromEmployees(db, e)
 	}
-
-	return nil
-
 }
 
 func main() {
 	db := database.MemoryDatabase{}
 
-	syncErr := synchronizeWrapperAndCache(&db)
-	if syncErr != nil {
-		fmt.Printf("error occured synchronizing: %q\n", syncErr.Error())
-	}
+	fmt.Println("Attempting to synchronize...")
+	synchronizeWrapperAndCache(&db)
+	fmt.Println("Done")
 
 	router := router.ConnectAPI(&db)
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
