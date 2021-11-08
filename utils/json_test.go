19fdfa3e8bf7b9474e743ca7ac598e5fb2cec381
pieter <tpho.skp@edu.mercantec.dk>
2021-11-08 15:00:33 +0100

    test

---
+++ utils/json_test.go
@@ -0,0 +1,39 @@
+package utils_test
+
+import (
+	"neocheckin_cache/utils"
+	"testing"
+)
+
+type exampleBody struct {
+	Name    string
+	Age     int
+	Pointer interface{}
+}
+
+func TestJsonEncode(t *testing.T) {
+	{
+		p := 20
+		e := exampleBody{
+			Name:    "scree",
+			Age:     10,
+			Pointer: &p,
+		}
+		_, err := utils.JsonEncode(e)
+		if err != nil {
+			t.Error("should not error with pointers")
+		}
+	}
+	{
+		p := "a"
+		e := exampleBody{
+			Name:    "scree",
+			Age:     10,
+			Pointer: p,
+		}
+		_, err := utils.JsonEncode(e)
+		if err != nil {
+			t.Errorf("should not error, %q", err.Error())
+		}
+	}
+}
