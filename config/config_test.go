50fa3017c195f5949df3ce3bc8eeb548cbde343e
pieter <tpho.skp@edu.mercantec.dk>
2021-11-09 09:32:32 +0100

    notice

--- config/config_test.go
+++ config/config_test.go
@@ -1,5 +1,18 @@
 package config_test
 
+/*
+note:
+
+TestRead will likely yield or fail because of it being "unable to find `config/settings.conf`"
+this is because when testing it will look for `config/settings.conf` in the `config` directory,
+because that is where the `config_test.go` file runs.
+which, errors because ofcourse it cannot find `config/config/settings.conf`
+if you want to pass the first test, simply create a file in `config/config/settings.conf`
+and add a line like `test=test`
+
+-tpho
+*/
+
 import (
 	c "neocheckin_cache/config"
 	"testing"
