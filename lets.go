5480dbb049526bb0e22d0a57243790ad3ae6d6a6
pieter <tpho.skp@edu.mercantec.dk>
2021-11-17 13:38:07 +0100

    logger

--- lets.go
+++ lets.go
@@ -5,17 +5,19 @@ import (
 	"log"
 	"neocheckin_cache/database"
 	"neocheckin_cache/router"
+	"neocheckin_cache/utils"
 	w "neocheckin_cache/wrapper"
 	"net/http"
 )
 
-// FIXME jeg ved ikke om funktionen virker
-func synchronizeWrapperAndCache(db database.AbstractDatabase) {
+// FIXME untested
+func synchronizeWrapperAndCache(db database.AbstractDatabase, l *utils.Logger) {
 
 	fmt.Println("Attempting to synchronize...")
 	t, err := w.GetTaskTypes()
 	if err != nil {
 		fmt.Printf("Error synchronizing task types: %v\n", err)
+		l.FormatAndAppendToLogFile(fmt.Sprintf("Error synchronizing task types: %v", err))
 	} else {
 		w.UpdateDbFromTaskTypes(db, t)
 	}
@@ -23,16 +25,17 @@ func synchronizeWrapperAndCache(db database.AbstractDatabase) {
 	e, err := w.GetEmployees()
 	if err != nil {
 		fmt.Printf("Error synchronizing employees: %v\n", err)
+		l.FormatAndAppendToLogFile(fmt.Sprintf("Error synchronizing employees: %v", err))
 	} else {
 		w.UpdateDbFromEmployees(db, e)
 	}
 	fmt.Println("Done")
 }
 
-func setupApiServer(db database.AbstractDatabase) {
+func setupApiServer(db database.AbstractDatabase, l *utils.Logger) {
 	router := router.ConnectAPI(db)
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		router.Handle(rw, *r, db)
+		router.Handle(rw, *r, db, l)
 	})
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
@@ -41,7 +44,10 @@ func setupApiServer(db database.AbstractDatabase) {
 }
 
 func main() {
+	logger := utils.Logger{}
+	logger.CreateLogFile()
+
 	db := database.MemoryDatabase{}
-	synchronizeWrapperAndCache(&db)
-	setupApiServer(&db)
+	synchronizeWrapperAndCache(&db, &logger)
+	setupApiServer(&db, &logger)
 }
